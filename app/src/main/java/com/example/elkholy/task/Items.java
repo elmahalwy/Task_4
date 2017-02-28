eb5ad2a21d7b18499bf343db496078aa5da6567b
Mohamed Elmahalwy <midoebrahim53@gmail.com>
2017-03-01 00:59:09 +0200

    New_change

--- app/src/main/java/com/example/elkholy/task/Items.java
+++ app/src/main/java/com/example/elkholy/task/Items.java
@@ -5,8 +5,45 @@ package com.example.elkholy.task;
  */
 
 public class Items {
-    String name;
-    public Items(String name){
-        this.name=name;
+    private int id;
+    private String name, url, status;
+
+    public Items(int id, String name, String status, String url) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
